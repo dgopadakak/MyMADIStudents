51c2426e697116b0cdfa0d318283a0c3511c74da
dgopadakak <dgopadakakv3@gmail.com>
2022-12-17 13:50:03 +0300

    The sorting function is done and tested

--- app/src/main/java/com/example/madistudents/ui/faculty/GroupOperator.kt
+++ app/src/main/java/com/example/madistudents/ui/faculty/GroupOperator.kt
@@ -1,5 +1,8 @@
 package com.example.madistudents.ui.faculty
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 class GroupOperator()
 {
     private var groups: ArrayList<Group> = ArrayList()
@@ -54,18 +57,159 @@ class GroupOperator()
             {
                 for (j in groups[indexGroup].listOfExams)
                 {
-                    if (i == j.nameOfExam)
+                    if (i == j.nameOfExam && !tempArrayListOfExams.contains(j))
                     {
                         tempArrayListOfExams.add(j)
+                        break
                     }
                 }
             }
             groups[indexGroup].listOfExams = tempArrayListOfExams
         }
+
         if (sortIndex == 1)
         {
-            val tempArrayListOfExamsNames: ArrayList<String> = ArrayList()
+            val tempArrayListOfTeacherNames: ArrayList<String> = ArrayList()
             val tempArrayListOfExams: ArrayList<Exam> = ArrayList()
+            for (i in groups[indexGroup].listOfExams)
+            {
+                tempArrayListOfTeacherNames.add(i.nameOfTeacher)
+            }
+            tempArrayListOfTeacherNames.sort()
+            for (i in tempArrayListOfTeacherNames)
+            {
+                for (j in groups[indexGroup].listOfExams)
+                {
+                    if (i == j.nameOfTeacher && !tempArrayListOfExams.contains(j))
+                    {
+                        tempArrayListOfExams.add(j)
+                        break
+                    }
+                }
+            }
+            groups[indexGroup].listOfExams = tempArrayListOfExams
+        }
+
+        if (sortIndex == 2)
+        {
+            val tempArrayListOfAuditory: ArrayList<Int> = ArrayList()
+            val tempArrayListOfExams: ArrayList<Exam> = ArrayList()
+            for (i in groups[indexGroup].listOfExams)
+            {
+                tempArrayListOfAuditory.add(i.auditory)
+            }
+            tempArrayListOfAuditory.sort()
+            for (i in tempArrayListOfAuditory)
+            {
+                for (j in groups[indexGroup].listOfExams)
+                {
+                    if (i == j.auditory && !tempArrayListOfExams.contains(j))
+                    {
+                        tempArrayListOfExams.add(j)
+                        break
+                    }
+                }
+            }
+            groups[indexGroup].listOfExams = tempArrayListOfExams
+        }
+
+        if (sortIndex == 3 || sortIndex == 4)
+        {
+            val tempArrayListOfTimeAndDate: ArrayList<GregorianCalendar> = ArrayList()
+            val tempArrayListOfExams: ArrayList<Exam> = ArrayList()
+            for (i in groups[indexGroup].listOfExams)
+            {
+                val d: List<String> = i.date.split(".")
+                val t: List<String> = i.time.split(":")
+                tempArrayListOfTimeAndDate.add(GregorianCalendar(d[2].toInt(), d[1].toInt(),
+                    d[0].toInt(), t[0].toInt(), t[1].toInt()))
+            }
+            tempArrayListOfTimeAndDate.sort()
+            for (i in tempArrayListOfTimeAndDate)
+            {
+                for (j in groups[indexGroup].listOfExams)
+                {
+                    val d: List<String> = j.date.split(".")
+                    val t: List<String> = j.time.split(":")
+                    val tempGregorianCalendar = GregorianCalendar(d[2].toInt(), d[1].toInt(),
+                        d[0].toInt(), t[0].toInt(), t[1].toInt())
+                    if (tempGregorianCalendar == i && !tempArrayListOfExams.contains(j))
+                    {
+                        tempArrayListOfExams.add(j)
+                        break
+                    }
+                }
+            }
+            groups[indexGroup].listOfExams = tempArrayListOfExams
+        }
+
+        if (sortIndex == 5)
+        {
+            val tempArrayListOfPeoples: ArrayList<Int> = ArrayList()
+            val tempArrayListOfExams: ArrayList<Exam> = ArrayList()
+            for (i in groups[indexGroup].listOfExams)
+            {
+                tempArrayListOfPeoples.add(i.peopleInAuditory)
+            }
+            tempArrayListOfPeoples.sort()
+            for (i in tempArrayListOfPeoples)
+            {
+                for (j in groups[indexGroup].listOfExams)
+                {
+                    if (i == j.peopleInAuditory && !tempArrayListOfExams.contains(j))
+                    {
+                        tempArrayListOfExams.add(j)
+                        break
+                    }
+                }
+            }
+            groups[indexGroup].listOfExams = tempArrayListOfExams
+        }
+
+        if (sortIndex == 6)
+        {
+            val tempArrayListOfAbstract: ArrayList<Int> = ArrayList()
+            val tempArrayListOfExams: ArrayList<Exam> = ArrayList()
+            for (i in groups[indexGroup].listOfExams)
+            {
+                tempArrayListOfAbstract.add(i.isAbstractAvailable)
+            }
+            tempArrayListOfAbstract.sort()
+            for (i in tempArrayListOfAbstract)
+            {
+                for (j in groups[indexGroup].listOfExams)
+                {
+                    if (i == j.isAbstractAvailable && !tempArrayListOfExams.contains(j))
+                    {
+                        tempArrayListOfExams.add(j)
+                        break
+                    }
+                }
+            }
+            groups[indexGroup].listOfExams = tempArrayListOfExams
+        }
+
+        if (sortIndex == 7)
+        {
+            val tempArrayListOfComment: ArrayList<String> = ArrayList()
+            val tempArrayListOfExams: ArrayList<Exam> = ArrayList()
+            for (i in groups[indexGroup].listOfExams)
+            {
+                tempArrayListOfComment.add(i.comment)
+            }
+            tempArrayListOfComment.sort()
+            for (i in tempArrayListOfComment)
+            {
+                for (j in groups[indexGroup].listOfExams)
+                {
+                    if (i == j.comment && !tempArrayListOfExams.contains(j))
+                    {
+                        tempArrayListOfExams.add(j)
+                        break
+                    }
+                }
+            }
+            groups[indexGroup].listOfExams = tempArrayListOfExams
         }
     }
 }
