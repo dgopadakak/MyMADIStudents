4d75327c48830dcbfa9f91e6b42f538c08d9e9b5
dgopadakak <dgopadakakv3@gmail.com>
2022-12-20 01:35:55 +0300

    A slight change in appearance and the addition of user-friendly functionality in exam details

--- app/src/main/java/com/example/madistudents/ExamDetailsDialogFragment.kt
+++ app/src/main/java/com/example/madistudents/ExamDetailsDialogFragment.kt
@@ -23,17 +23,27 @@ class ExamDetailsDialogFragment: android.app.DialogFragment()
 
     lateinit var onInputListenerSortId: OnInputListenerSortId
 
+    private lateinit var textViewExamNameTitle: TextView
     private lateinit var textViewExamName: TextView
+    private lateinit var textViewTeacherNameTitle: TextView
     private lateinit var textViewTeacherName: TextView
+    private lateinit var textViewAuditoryTitle: TextView
     private lateinit var textViewAuditory: TextView
+    private lateinit var textViewDateTitle: TextView
     private lateinit var textViewDate: TextView
+    private lateinit var textViewTimeTitle: TextView
     private lateinit var textViewTime: TextView
+    private lateinit var textViewPeopleTitle: TextView
     private lateinit var textViewPeople: TextView
+    private lateinit var textViewAbstractTitle: TextView
     private lateinit var textViewAbstract: TextView
+    private lateinit var textViewCommentTitle: TextView
     private lateinit var textViewComment: TextView
     private lateinit var buttonDel: Button
     private lateinit var buttonEdit: Button
+    private lateinit var buttonCancel: Button
     private lateinit var buttonOk: Button
+    private lateinit var textViewCurrSort: TextView
 
     private var currentIdForSort: Int = -1
 
@@ -41,29 +51,48 @@ class ExamDetailsDialogFragment: android.app.DialogFragment()
                               savedInstanceState: Bundle?): View
     {
         val view: View = inflater!!.inflate(R.layout.exam_details, container, false)
+        textViewExamNameTitle = view.findViewById(R.id.textViewExamNameTitle)
         textViewExamName = view.findViewById(R.id.textViewExamName)
+        textViewTeacherNameTitle = view.findViewById(R.id.textViewTeacherNameTitle)
         textViewTeacherName = view.findViewById(R.id.textViewTeacherName)
+        textViewAuditoryTitle = view.findViewById(R.id.textViewAuditoryTitle)
         textViewAuditory = view.findViewById(R.id.textViewAuditory)
+        textViewDateTitle = view.findViewById(R.id.textViewDateTitle)
         textViewDate = view.findViewById(R.id.textViewDate)
+        textViewTimeTitle = view.findViewById(R.id.textViewTimeTitle)
         textViewTime = view.findViewById(R.id.textViewTime)
+        textViewPeopleTitle = view.findViewById(R.id.textViewPeopleTitle)
         textViewPeople = view.findViewById(R.id.textViewPeople)
+        textViewAbstractTitle = view.findViewById(R.id.textViewAbstractTitle)
         textViewAbstract = view.findViewById(R.id.textViewAbstract)
+        textViewCommentTitle = view.findViewById(R.id.textViewCommentTitle)
         textViewComment = view.findViewById(R.id.textViewComment)
         buttonDel = view.findViewById(R.id.button_details_delete)
         buttonEdit = view.findViewById(R.id.button_details_edit)
+        buttonCancel = view.findViewById(R.id.button_cancel)
         buttonOk = view.findViewById(R.id.button_details_ok)
+        textViewCurrSort = view.findViewById(R.id.textViewCurrentSort)
 
+        textViewExamNameTitle.setOnLongClickListener { setSortId(0) }
         textViewExamName.setOnLongClickListener { setSortId(0) }
+        textViewTeacherNameTitle.setOnLongClickListener { setSortId(1) }
         textViewTeacherName.setOnLongClickListener { setSortId(1) }
+        textViewAuditoryTitle.setOnLongClickListener { setSortId(2) }
         textViewAuditory.setOnLongClickListener { setSortId(2) }
+        textViewDateTitle.setOnLongClickListener { setSortId(3) }
         textViewDate.setOnLongClickListener { setSortId(3) }
+        textViewTimeTitle.setOnLongClickListener { setSortId(4) }
         textViewTime.setOnLongClickListener { setSortId(4) }
+        textViewPeopleTitle.setOnLongClickListener { setSortId(5) }
         textViewPeople.setOnLongClickListener { setSortId(5) }
+        textViewAbstractTitle.setOnLongClickListener { setSortId(6) }
         textViewAbstract.setOnLongClickListener { setSortId(6) }
+        textViewCommentTitle.setOnLongClickListener { setSortId(7) }
         textViewComment.setOnLongClickListener { setSortId(7) }
 
         buttonDel.setOnClickListener { returnDel() }
         buttonEdit.setOnClickListener { returnEdit() }
+        buttonCancel.setOnClickListener { dialog.dismiss() }
         buttonOk.setOnClickListener { returnIdForSort() }
 
         val arguments: Bundle = getArguments()
@@ -106,6 +135,34 @@ class ExamDetailsDialogFragment: android.app.DialogFragment()
     private fun setSortId(id: Int): Boolean
     {
         currentIdForSort = id
+        if (currentIdForSort == 0)
+        {
+            textViewCurrSort.text = "Сортировка по предмету"
+        }
+        else if (currentIdForSort == 1)
+        {
+            textViewCurrSort.text = "Сортировка по ФИО"
+        }
+        else if (currentIdForSort == 2)
+        {
+            textViewCurrSort.text = "Сортировка по аудитории"
+        }
+        else if (currentIdForSort == 3 || currentIdForSort == 4)
+        {
+            textViewCurrSort.text = "Сортировка по дате и времени"
+        }
+        else if (currentIdForSort == 5)
+        {
+            textViewCurrSort.text = "Сортировка по количеству"
+        }
+        else if (currentIdForSort == 6)
+        {
+            textViewCurrSort.text = "Сортировка по лекциям"
+        }
+        else if (currentIdForSort == 7)
+        {
+            textViewCurrSort.text = "Сортировка по комментарию"
+        }
         val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         vibrator.vibrate(VibrationEffect.createOneShot(200
             , VibrationEffect.DEFAULT_AMPLITUDE))
